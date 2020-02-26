477f80ad8dbb0cf8e78102820a827b544a7f0831
David Carlisle <d.p.carlisle@gmail.com>
2020-02-26 20:37:10 +0000

    update auto tag to match luacolor

--- build.lua
+++ build.lua
@@ -41,9 +41,11 @@ if tagname == 'auto' then
     newv = tagdate .. ' v'  .. a .. math.floor(b + 1)
     print('USING OLD TAG: ' .. oldv)
     print('USING NEW TAG: ' .. newv)
-    content=string.gsub(content,"{Version}{" .. oldv,'##OLDV##')
-    content=string.gsub(content,string.gsub(oldv,"[-/]", "[-/]"),newv)
+    local oldpattern = string.gsub(oldv,"[-/]", "[-/]")
+    content=string.gsub(content,"{Version}{" .. oldpattern,'##OLDV##')
+    content=string.gsub(content,oldpattern,newv)
     content=string.gsub(content,'##OLDV##',"{Version}{" .. oldv)
+    content=string.gsub(content,'%-%d%d%d%d Oberdiek Package','-' .. os.date("%Y") .. " Oberdiek Package")
     content = string.gsub(content,
         '%% \\end{History}',
 	'%%   \\begin{Version}{' .. newv .. '}\n%%   \\item Updated\n%%   \\end{Version}\n%% \\end{History}')
